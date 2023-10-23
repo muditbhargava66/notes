d186507005f24c36edc3b22205f7349ea7d75a16
Mudit Bhargava <mudit@Mudits-MacBook-Pro.local>
2023-10-23 16:08:33 -0400

    Head changes

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -12,8 +12,7 @@ export default (() => {
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
 
-    const iconPath = joinSegments(baseDir, "static/icon.png")
-    const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
+    const fontStylePath = joinSegments(baseDir, "static/font/cmun.css")
 
     return (
       <head>
@@ -22,14 +21,13 @@ export default (() => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
         <meta property="og:width" content="1200" />
         <meta property="og:height" content="675" />
-        <link rel="icon" href={iconPath} />
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href={fontStylePath} rel="stylesheet" type="text/css" spa-preserve />
         {css.map((href) => (
           <link key={href} href={href} rel="stylesheet" type="text/css" spa-preserve />
         ))}

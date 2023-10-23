8a79b970934f167fc95cdeda784c2fbc6bf75dc2
Mudit Bhargava <mudit@Mudits-MacBook-Pro.local>
2023-10-23 16:28:10 -0400

    Contact Links added

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,9 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/muditbhargava66",
+      "LinkedIn": "https://www.linkedin.com/in/mudit-b07/",
+      "E-mail": "mb8630@nyu.edu" 
     },
   }),
 }

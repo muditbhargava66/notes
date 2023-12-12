f16eb41b5f7a7325f9f6aa36daab93ef0fd8eb3b
Mudit Bhargava <mudit@Mudits-MacBook-Pro.local>
2023-12-12 16:30:48 -0500

    Quartz sync: Dec 12, 2023, 4:30 PM

--- quartz/components/MobileOnly.tsx
+++ quartz/components/MobileOnly.tsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 export default ((component?: QuartzComponent) => {
   if (component) {
-    const Component = component
+    const Component = component;
     function MobileOnly(props: QuartzComponentProps) {
-      return <Component displayClass="mobile-only" {...props} />
+      const [isOpen, setIsOpen] = useState(false);
+      return (
+        <>
+          <button onClick={() => setIsOpen(true)}>
+            Open Drawer
+          </button>
+          <div className={`drawer ${isOpen ? 'open' : ''}`}>
+            <Component {...props} />
+            <button onClick={() => setIsOpen(false)}>
+              Close
+            </button>
+          </div>
+        </>
+      )
     }
 
     MobileOnly.displayName = component.displayName
